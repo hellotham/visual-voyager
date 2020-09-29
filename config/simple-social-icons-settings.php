793a278e1a3631d0761aaba8089599ae955d75b4
Christine Tham <chris.tham@hellotham.com>
2020-09-29 15:47:09 +1000

    Fixed minor issues

--- config/simple-social-icons-settings.php
+++ config/simple-social-icons-settings.php
@@ -12,5 +12,7 @@ return [
 	'alignment'              => 'alignright',
 	'border_radius'          => 3,
 	'border_width'           => 0,
+	'icon_color'			 => '#d2386c',
+	'icon_hover_color'		 => '#b565a7',
 	'size'                   => 40,
 ];
